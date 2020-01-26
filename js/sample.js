a546f9f7b65c5ef2d5cb25f815948137a0aedffb
Gobi-Chrono <gobinath.chronoinfotech@gmail.com>
2020-01-26 14:07:45 +0530

    Updated Dark mode and light mode

--- js/sample.js
+++ js/sample.js
@@ -26,6 +26,30 @@ iamharsh.set({
  window.addEventListener('load', function(){
  	setTimeout(function(){
 overlay.style.display = 'none';
- 	},1000)
+ 	},2000)
  
  })
+
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {        document.documentElement.setAttribute('data-theme', 'light');
+          localStorage.setItem('theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
